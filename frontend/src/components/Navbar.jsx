11fdd1050125880cf45c74432f0bd1c573ddd987
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-12-01 14:36:33 +0530

    navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <div className="flex items-center gap-2">
             <Link to={"/settings"} className={`btn btn-sm gap-2 transition-colors`}>
               <Settings className="w-4 h-4" />
-              <span className="hidden sm:inline">Settings</span>
+              <span className="hidden sm:inline">Themes</span>
             </Link>
 
             {authUser && (
