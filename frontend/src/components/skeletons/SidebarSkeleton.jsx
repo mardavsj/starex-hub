95e5ff82b6ad5e6b60005f7b6fe3851c299880d9
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-12-08 20:35:22 +0530

    sidebar skeleton

--- frontend/src/components/skeletons/SidebarSkeleton.jsx
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -4,12 +4,12 @@ const SidebarSkeleton = () => {
     const skeletonContacts = Array(8).fill(null);
 
     return (
-        <aside className="h-full w-20 lg:w-72 border-r border-base-300 
+        <aside className="h-full w-full lg:w-72 border-r border-base-300 
     flex flex-col transition-all duration-200">
             <div className="border-b border-base-300 w-full p-5">
                 <div className="flex items-center gap-2">
                     <Users className="w-6 h-6" />
-                    <span className="font-medium hidden lg:block">Contacts</span>
+                    <span className="font-medium">All Contacts</span>
                 </div>
             </div>
 
@@ -17,11 +17,11 @@ const SidebarSkeleton = () => {
                 {skeletonContacts.map((_, idx) => (
                     <div key={idx} className="w-full p-3 flex items-center gap-3">
 
-                        <div className="relative mx-auto lg:mx-0">
+                        <div className="relative mx-auto lg:mx">
                             <div className="skeleton size-12 rounded-full" />
                         </div>
 
-                        <div className="hidden lg:block text-left min-w-0 flex-1">
+                        <div className="text-left min-w-0 flex-1">
                             <div className="skeleton h-4 w-32 mb-2" />
                             <div className="skeleton h-3 w-16" />
                         </div>
