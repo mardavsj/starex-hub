4f4d3d4b687783ccac255cd89dbd733ea21287b3
Mardav Jadaun <mardavjadaun113@gmail.com>
2024-12-08 18:22:25 +0530

    sidebar updated

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -152,9 +152,9 @@ const Sidebar = () => {
             <div className="border-b border-base-300 w-full p-5">
                 <div className="flex items-center gap-2">
                     <Users className="size-6" />
-                    <span className="font-medium hidden lg:block">Contacts</span>
+                    <span className="font-medium">All Contacts</span>
                 </div>
-                <div className="mt-3 hidden lg:flex items-center gap-2">
+                <div className="mt-3 flex items-center gap-2">
                     <label className="cursor-pointer flex items-center gap-2">
                         <input
                             type="checkbox"
